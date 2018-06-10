397e85def6763ab2bf5784bbe97b9b7a5efdbf18
dekken <philip.deegan@gmail.com>
2018-06-10 19:55:48 +0200

    clean

--- inc/kul/serial/cli.bottom.hpp
+++ inc/kul/serial/cli.bottom.hpp
@@ -32,11 +32,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define _KUL_SERIAL_CLI_BOTTOM_HPP_
 
 #if defined(_MKN_WITH_IO_CEREAL_)
-
 CEREAL_SPECIALIZE_FOR_ALL_ARCHIVES(kul::cli::Args,
                                    cereal::specialization::member_load_save)
-// CEREAL_REGISTER_TYPE(kul::cli::Args)
-
 #endif  //  _MKN_WITH_IO_CEREAL_
 
 #endif  // _KUL_SERIAL_CLI_BOTTOM_HPP_
