278004cad9dc363043cd5923b1f2d2fd0836868b
dekken <philip.deegan@gmail.com>
2018-06-15 19:43:15 +0200

    xcode 9 issues with pthread on mac +

--- os/bsd/inc/kul/def.hpp
+++ os/bsd/inc/kul/def.hpp
@@ -43,6 +43,14 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #if defined(__APPLE__)
 
+#ifndef _MACH_PORT_T
+#define _MACH_PORT_T
+#include <sys/_types.h> /* __darwin_mach_port_t */
+typedef __darwin_mach_port_t mach_port_t;
+#include <pthread.h>
+mach_port_t pthread_mach_thread_np(pthread_t);
+#endif /* _MACH_PORT_T */
+
 #ifndef _XOPEN_SOURCE
 #define _XOPEN_SOURCE 700
 #endif  //_XOPEN_SOURCE
