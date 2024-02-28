58c9f102841405eb4f76641363e86b932791a7e9
PhilipDeegan <philip.deegan@gmail.com>
2024-02-28 16:04:08 +0000

    and -> && for msvc (#46)

--- inc/mkn/kul/os/win/env.hpp
+++ inc/mkn/kul/os/win/env.hpp
@@ -84,7 +84,7 @@ inline std::string GET(char const* c, std::string default_ = "") {
 }
 
 inline void SET(char const* var, char const* val = nullptr) {
-  if (val and strlen(val) > 0)
+  if (val && strlen(val) > 0)
     _putenv(std::string(std::string(var) + "=" + std::string(val)).c_str());
   else  // unset
     _putenv(std::string(std::string(var) + "=").c_str());
