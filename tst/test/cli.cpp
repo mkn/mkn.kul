ac10ceeb09c6864d32cf0dd8c32edeeec22f35a5
PhilipDeegan <philip.deegan@gmail.com>
2026-01-28 20:18:22 +0100

    ++

--- tst/test/cli.cpp
+++ tst/test/cli.cpp
@@ -13,3 +13,12 @@ TEST(CLI_Test, ParseCommandLineArguments) {
   EXPECT_EQ("words not in quotes", v[2]);
   EXPECT_EQ("end", v[3]);
 }
+
+TEST(CLI_Test, ParseCommandPythonExample) {
+  std::vector<std::string> v =
+      mkn::kul::cli::asArgs("python3 -c \"import sys; print('lol ' + dir(sys)\"");
+  ASSERT_EQ(3ull, v.size());
+  EXPECT_EQ("python3", v[0]);
+  EXPECT_EQ("-c", v[1]);
+  EXPECT_EQ("import sys; print('lol ' + dir(sys)", v[2]);
+}
