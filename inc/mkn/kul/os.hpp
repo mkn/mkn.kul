03e2061326992cfc035ee86d78de4eedb78daf82
PhilipDeegan <philip.deegan@gmail.com>
2026-08-08 21:38:53 +0200

    ++

--- inc/mkn/kul/os.hpp
+++ inc/mkn/kul/os.hpp
@@ -205,9 +205,7 @@ class File : public fs::Item {
 
  public:
   File() : _n(), _d() {}
-  File(std::string const& n, bool m = false) : _n(Dir::LOCL(n)) {
-    // _d starts empty so is() stats _n as-given (Dir::join returns it unprefixed),
-    // correctly resolving both absolute and CWD-relative input before any CWD default is applied.
+  File(std::string const& n, bool m = false) : _n(Dir::LOCL(n)), _d(env::CWD()) {
     if (is()) {
       try {
         this->_n = Dir::REAL(this->_n);
@@ -215,13 +213,13 @@ class File : public fs::Item {
         this->_n = this->_n.substr(_d.path().size() + 1);
       } catch (mkn::kul::fs::Exception const& e) {
       }
-    } else if (n.find(Dir::SEP()) != std::string::npos) {
-      this->_d = Dir(n.substr(0, n.rfind(Dir::SEP())));
-      this->_n = this->_n.substr(n.rfind(Dir::SEP()) + 1);
-    } else if (auto const& _N = Dir::REAL_OR_NULL(this->_n)) {
-      this->_d = Dir(Dir::PRNT(*_N), m);
     } else {
-      this->_d = Dir(env::CWD());
+      if (n.find(Dir::SEP()) != std::string::npos) {
+        this->_d = Dir(n.substr(0, n.rfind(Dir::SEP())));
+        this->_n = this->_n.substr(n.rfind(Dir::SEP()) + 1);
+      } else {
+        if (auto const& _N = Dir::REAL_OR_NULL(this->_n)) this->_d = Dir(Dir::PRNT(*_N), m);
+      }
     }
   }
   File(char const* n, bool m = false) : File(std::string(n), m) {}
--- tst/test/os.cpp
+++ tst/test/os.cpp
@@ -39,3 +39,60 @@ TEST(OperatingSystemTests, HasFileModifiedTimeStampSupport) {
   ASSERT_TRUE(TimeStampHandler::INSTANCE().is());
   ASSERT_TRUE(TimeStampHandler::INSTANCE().timeStamps().modified());
 }
+
+TEST(OperatingSystemTests, FileAbsolutePathMatchesRelativeForTopLevelFile) {
+  mkn::kul::File const relative("mkn.yaml");
+  ASSERT_TRUE(relative.is());
+  mkn::kul::File const absolute(relative.real());
+  EXPECT_TRUE(absolute.is());
+  EXPECT_EQ(relative.name(), absolute.name());
+  EXPECT_EQ(relative.real(), absolute.real());
+  EXPECT_EQ(relative.dir().real(), absolute.dir().real());
+  EXPECT_EQ(relative.size(), absolute.size());
+}
+
+TEST(OperatingSystemTests, FileAbsolutePathMatchesRelativeForNestedFile) {
+  mkn::kul::File const relative("inc/mkn/kul/os.hpp");
+  ASSERT_TRUE(relative.is());
+  mkn::kul::File const absolute(relative.real());
+  EXPECT_TRUE(absolute.is());
+  EXPECT_EQ(relative.name(), absolute.name());
+  EXPECT_EQ(relative.real(), absolute.real());
+  EXPECT_EQ(relative.dir().real(), absolute.dir().real());
+  EXPECT_EQ(relative.size(), absolute.size());
+}
+
+TEST(OperatingSystemTests, FileAbsolutePathResolvesRegardlessOfCWD) {
+  mkn::kul::File const relative("LICENSE.md");
+  ASSERT_TRUE(relative.is());
+  std::string const absPath = relative.real();
+
+  mkn::kul::os::PushDir const pd(mkn::kul::Dir("inc"));
+  mkn::kul::File const absolute(absPath);
+  EXPECT_TRUE(absolute.is());
+  EXPECT_EQ(absPath, absolute.real());
+}
+
+TEST(OperatingSystemTests, FileAbsolutePathIsCanonicalized) {
+  std::string const messy =
+      mkn::kul::env::CWD() + mkn::kul::Dir::SEP() + mkn::kul::Dir::SEP() + "mkn.yaml";
+  mkn::kul::File const f(messy);
+  ASSERT_TRUE(f.is());
+  EXPECT_EQ("mkn.yaml", f.name());
+  EXPECT_EQ(mkn::kul::Dir(mkn::kul::env::CWD()).path(), f.dir().path());
+}
+
+TEST(OperatingSystemTests, FileAbsoluteNonExistentPathSplitsDirAndName) {
+  std::string const absPath =
+      mkn::kul::env::CWD() + mkn::kul::Dir::SEP() + "does_not_exist_kul_os_test.txt";
+  mkn::kul::File const f(absPath);
+  EXPECT_FALSE(f.is());
+  EXPECT_EQ("does_not_exist_kul_os_test.txt", f.name());
+  EXPECT_EQ(mkn::kul::env::CWD(), f.dir().path());
+}
+
+TEST(OperatingSystemTests, FileBareNonExistentNameDefaultsToCWD) {
+  mkn::kul::File const f("does_not_exist_kul_os_test.txt");
+  EXPECT_FALSE(f.is());
+  EXPECT_EQ(mkn::kul::Dir(mkn::kul::env::CWD()).real(), f.dir().real());
+}
